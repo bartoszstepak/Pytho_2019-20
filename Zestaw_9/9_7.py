707476f5e79ed721ec3b635f10f51285b0c0cd9e
bartoszstepak <46901072+bartoszstepak@users.noreply.github.com>
2019-12-12 23:39:33 +0100

    Update 9_7.py

--- Zestaw_9/9_7.py
+++ Zestaw_9/9_7.py
@@ -1,11 +1,22 @@
 def bst_min(top):
+    if top is None:
+        rise ValueError("Three is empty");
+        
     min = top
+    
     while min.left is not None:
         min = min.left
+        
     return min
 
+
 def bst_max(top):
+    if top is None:
+        rise ValueError("Three is empty");
+    
     max = top
+    
     while max.right is not None:
         max = max.right;
+    
     return max
